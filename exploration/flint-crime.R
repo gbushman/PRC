ee13c99e2c4df5d5653b73c9ba082672653ca5ed
Bushman <gbushman@UMICH.EDU>
2017-10-06 17:16:42 -0400

    edits to crime mapping script

--- exploration/flint-crime.R
+++ exploration/flint-crime.R
@@ -5,26 +5,31 @@ library(tidyverse)
 library(forcats)
 library(haven)
 library(magrittr)
+library(spatstat)
 library(leaflet)
 library(Hmisc)
 library(ggmap)
 library(sf)
 
 ## load data
-crime <- read_spss("U:/HBHE/PRC/Projects/YVPC 2000-2005/Crime Data/Jianan's Working File
-                   /2015 Flint/2015 Crime Data/Flint_Crime_Data_2015.sav")
+crime <- read_spss("U:/HBHE/PRC/Projects/YVPC 2000-2005/Crime Data/Jianan's Working File/2015 Flint/2015 Crime Data/Flint_Crime_Data_2015.sav")
 
 colnames(crime) <- colnames(crime) %>% tolower
-crime$filec_nibrs_desc <- factor(crime$filec_nibrs_desc)
 
 
 # Visualize Data ----------------------------------------------------------
 ## kernel density
-X <- cbind(lng,lat)
-kde2d <- bkde2D(X, bandwidth=c(bw.ucv(X[,1]),bw.ucv(X[,2])))
+# crime <- crime %>%
+#   filter(!is.na(latitude),
+#          !is.na(longitude)) %>%
+#   select(latitude, longitude) %>%
+#   as.data.frame()
+# 
+# sp <- as(crime, "SpatialPoints")
+
 
 ## mapping
-leaflet(data = crime ) %>%
+leaflet(data = crime) %>%
   addProviderTiles(providers$CartoDB.Positron) %>%
   addCircleMarkers(
     ~longitude, 
